3a7025c0de6df79c467c821fd32b55a06aa884d3
ambucha <ambuchatskiy@gmail.com>
2025-10-05 19:48:23 -0700

    Thrid commit

--- lab18.cpp
+++ lab18.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // create the node struct with the data needed, in this case the rating and the comment
 struct Node{
-    int rating;
+    double rating;
     string comment;
     Node *next;
 };
@@ -22,19 +22,20 @@ void output(Node *);
 // pushback(): pushes value to the back of the linked list
 // arguments: pointer to node, value, comment
 // returns: nothing
-void pushback(Node *, int, string);
+void pushback(Node *, double, string);
 
-// pushback(): pushes value to the front of the linked list
+// pushfront(): pushes value to the front of the linked list
 // arguments: pointer to node, value, comment
 // returns: nothing
-void pushback(Node *, int, string);
+void pushfront(Node *, double, string);
 
 int main(){
     //variables
     Node *head = nullptr;
     int method;
-    int rating;
+    double rating;
     string comment; 
+    char yn;
 
     // ask for if to push to front or back
     cout << "Which linked list method should we use?" << endl;
@@ -46,15 +47,82 @@ int main(){
     // Check for chosen method (and if neither is chosen)
     if(method == 1){
         // ask for review rating and comment
-        cout << "";
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cout << "Enter review comments: ";
+        cin.ignore();
+        getline(cin, comment);
+        pushfront(head, rating, comment);
 
+        // check if they want to keep going
+        cout << "Enter another review? Y/N: ";
+        cin >> yn;
+
+        // if they want to keep going add a while loop for however long they want to keep going
+        while(yn == 'y' || yn == 'Y'){
+            cout << "Enter review rating 0-5: ";
+            cin >> rating;
+            cout << "Enter review comments: ";
+            cin.ignore();
+            getline(cin, comment);
+            pushfront(head, rating, comment);
+            
+            // recheck if they want to keep going to make sure its not inifinite
+            cout << "Enter another review? Y/N: ";
+            cin >> yn;
+        }
     }
     else if(method == 2) {
+        // ask for review rating and comment
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cout << "Enter review comments: ";
+        cin.ignore();
+        getline(cin, comment);
+        pushback(head, rating, comment);
 
+        // check if they want to keep going
+        cout << "Enter another review? Y/N: ";
+        cin >> yn;
+
+        // if they want to keep going add a while loop for however long they want to keep going
+        while(yn == 'y' || yn == 'Y'){
+            cout << "Enter review rating 0-5: ";
+            cin >> rating;
+            cout << "Enter review comments: ";
+            cin.ignore();
+            getline(cin, comment);
+            pushback(head, rating, comment);
+            
+            // recheck if they want to keep going to make sure its not inifinite
+            cout << "Enter another review? Y/N: ";
+            cin >> yn;
+        }
     }
     else {
         cout << "Error: Neither method chosen" << endl;
     }
 
+    // output the lists:
+    output(head);
+
     return 0;
 }
+
+void output(Node * hd){
+    if (!hd){
+        cout << "Empty list\n";
+        return;
+    }
+    int count = 1;
+    Node * current = hd;
+}
+
+void pushback(Node * hd, double r, string c){
+
+}
+
+void pushfront(Node * hd, double r, string c){
+
+}
+
